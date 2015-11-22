16409e046a0ed40d4c17beb885d1d4d9d9b6e7d7
SangeetShah93 <sangeetshah93@gmail.com>
2015-11-22 17:29:56 +0000

    Logo Constraints Changed
    Added:
    
    Logo/View CenterX Constraint
    Logo/View Top Constraint
    Logo Height Constraint
    Logo Width Constraint
    Logo ContentMode = Aspect Fit
    
    Removed:
    
    Storyboard Logo imageView and associated storyboard constraints

--- Break in2/HomeViewController.swift
+++ Break in2/HomeViewController.swift
@@ -11,20 +11,64 @@ import QuartzCore
 
 class HomeViewController: UIViewController {
 
-    //Declare all objects
-    @IBOutlet weak var logo: UIImageView!
-    @IBOutlet weak var profilePic: UIImageView!
-    @IBOutlet weak var buttonBack: UIImageView!
-    @IBOutlet weak var buttonIB: UIButton!
-    @IBOutlet weak var buttonTrading: UIButton!
+  //Declare all objects
+  @IBOutlet weak var logo: UIImageView!
+  @IBOutlet weak var profilePic: UIImageView!
+  @IBOutlet weak var buttonBack: UIImageView!
+  @IBOutlet weak var buttonIB: UIButton!
+  @IBOutlet weak var buttonTrading: UIButton!
+  
+  // Declare and initialize views
+  let logoImageView:UIImageView = UIImageView()
+  let profilePictureImageView:UIImageView = UIImageView()
+  let backButton:UIButton = UIButton()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.addHomeBG() //add BG image
-        // Do any additional setup after loading the view.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+      
+    // Do any additional setup after loading the view.
+      
+    // Add background image to HomeViewController's view
+      
+    self.view.addHomeBG()
+    
+    // Add logoImageView and profilePictureImageView to HomeViewController view
+    
+    self.view.addSubview(self.logoImageView)
+    self.view.addSubview(self.profilePictureImageView)
+    
+    // Customize and add content to imageViews
+    
+    self.logoImageView.contentMode = UIViewContentMode.ScaleAspectFit 
+    self.logoImageView.image = UIImage.init(named: "textBreakIn2Small")
+    
+    // Set constraints
+    
+    self.setConstraints()
+    
+    
+  }
+  
+  func setConstraints() {
+    
+    // Create and add constraints for logoImageView
+    
+    self.logoImageView.translatesAutoresizingMaskIntoConstraints = false
+    
+    let logoImageViewCenterConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logoImageView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
+    
+    let logoImageViewTopConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logoImageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 25)
+    
+    let logoImageViewHeightConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logoImageView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 50)
+    
+    let logoImageViewWidthConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logoImageView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
+    
+    self.logoImageView.addConstraints([logoImageViewHeightConstraint, logoImageViewWidthConstraint])
+    self.view.addConstraints([logoImageViewCenterConstraint, logoImageViewTopConstraint])
+    
+  }
 
-    override func didReceiveMemoryWarning() {
+  override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
