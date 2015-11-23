52cf7e9c832e4974281166656d7b000dc8558691
SangeetShah93 <sangeetshah93@gmail.com>
2015-11-23 19:03:03 +0000

    Further Constraint Changes
    Added:
    
    ProfilePicture/View CenterX Constraint
    ProfilePicture/View Top Constraint
    ProfilePicture Height Constraint
    ProfilePicture Width Constraint
    
    BackButton/View Top Constraint
    BackButton/View Left Constraint
    BackButton Height Constraint
    BackButton Width Constraint
    
    SettingsButton/View Top Constraint
    SettingsButton/View Right Constraint
    SettingsButton Height Constraint
    SettingsButton Width Constraint
    
    LogOutButton/View Bottom Constraint
    LogOutButton/View Left Constraint
    LogOutButton/View Right Constraint
    LogOutButton/ProfilePicture Top Constraint
    
    CareersScrollView/ProfilePicture Top Constraint
    CareersScrollView/View Left Constraint
    CareersScrollView/View Right Constraint
    CareersScrollView/LogOutButton Bottom Constraint
    
    Removed:
    
    Storyboard BackButton and associated constraints
    Storyboard SettingsButton and associated constraints
    Storyboard ProfilePicture ImageView and associated constraints
    Storyboard LogOutButton and associated constraints

--- Break in2/HomeViewController.swift
+++ Break in2/HomeViewController.swift
@@ -12,21 +12,35 @@ import QuartzCore
 class HomeViewController: UIViewController {
 
   //Declare all objects
-  @IBOutlet weak var logo: UIImageView!
-  @IBOutlet weak var profilePic: UIImageView!
+  
   @IBOutlet weak var buttonBack: UIImageView!
   @IBOutlet weak var buttonIB: UIButton!
   @IBOutlet weak var buttonTrading: UIButton!
   
   // Declare and initialize views
+  
   let logoImageView:UIImageView = UIImageView()
   let profilePictureImageView:UIImageView = UIImageView()
   let backButton:UIButton = UIButton()
+  let settingsButton:UIButton = UIButton()
+  let logOutButton:UIButton = UIButton()
+  let careersScrollView:UIScrollView = UIScrollView()
+  
+  // Declare and initialize design constants
+  
+  let statusBarFrame:CGRect = UIApplication.sharedApplication().statusBarFrame
+  
+  let majorMargin:CGFloat = 20
+  let minorMargin:CGFloat = 10
     
   override func viewDidLoad() {
     super.viewDidLoad()
       
     // Do any additional setup after loading the view.
+    
+    // Declare and initialize types of careers
+    
+    let careerTypes:[String] = ["Investment Banking", "Engineering", "Trading"]
       
     // Add background image to HomeViewController's view
       
@@ -36,11 +50,30 @@ class HomeViewController: UIViewController {
     
     self.view.addSubview(self.logoImageView)
     self.view.addSubview(self.profilePictureImageView)
+    self.view.addSubview(self.backButton)
+    self.view.addSubview(self.settingsButton)
+    self.view.addSubview(self.logOutButton)
+    self.view.addSubview(self.careersScrollView)
     
     // Customize and add content to imageViews
     
     self.logoImageView.contentMode = UIViewContentMode.ScaleAspectFit 
     self.logoImageView.image = UIImage.init(named: "textBreakIn2Small")
+    
+    self.profilePictureImageView.backgroundColor = UIColor.whiteColor()
+    
+    self.backButton.setImage(UIImage.init(named: "back"), forState: UIControlState.Normal)
+    
+    self.settingsButton.setImage(UIImage.init(named: "settings"), forState: UIControlState.Normal)
+    
+    self.logOutButton.backgroundColor = UIColor.lightGrayColor()
+    self.logOutButton.setTitle("Log Out", forState: UIControlState.Normal)
+    
+    self.careersScrollView.backgroundColor = UIColor.whiteColor()
+    
+    // Customize careersScrollView
+    
+    self.careersScrollView.showsVerticalScrollIndicator = false
     
     // Set constraints
     
@@ -55,7 +88,7 @@ class HomeViewController: UIViewController {
     
     self.logoImageView.translatesAutoresizingMaskIntoConstraints = false
     
-    let logoImageViewCenterConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logoImageView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
+    let logoImageViewCenterXConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logoImageView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
     
     let logoImageViewTopConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logoImageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 25)
     
@@ -64,7 +97,81 @@ class HomeViewController: UIViewController {
     let logoImageViewWidthConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logoImageView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
     
     self.logoImageView.addConstraints([logoImageViewHeightConstraint, logoImageViewWidthConstraint])
-    self.view.addConstraints([logoImageViewCenterConstraint, logoImageViewTopConstraint])
+    self.view.addConstraints([logoImageViewCenterXConstraint, logoImageViewTopConstraint])
+    
+    // Create and add constraints for profilePictureImageView
+    
+    self.profilePictureImageView.translatesAutoresizingMaskIntoConstraints = false
+    
+    let profilePictureImageViewCenterXConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.profilePictureImageView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
+    
+    let profilePictureImageViewTopConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.profilePictureImageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self.logoImageView, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: self.majorMargin)
+    
+    let profilePictureImageViewHeightConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.profilePictureImageView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 150)
+    
+    let profilePictureImageViewWidthConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.profilePictureImageView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 150)
+    
+    self.profilePictureImageView.addConstraints([profilePictureImageViewHeightConstraint, profilePictureImageViewWidthConstraint])
+    self.view.addConstraints([profilePictureImageViewCenterXConstraint, profilePictureImageViewTopConstraint])
+    
+    // Create and add constraints for backButton
+    
+    self.backButton.translatesAutoresizingMaskIntoConstraints = false
+    
+    let backButtonLeftConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.backButton, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: self.minorMargin)
+    
+    let backButtonTopConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.backButton, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: self.statusBarFrame.height + self.minorMargin)
+    
+    let backButtonHeightConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.backButton, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 30)
+    
+    let backButtonWidthConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.backButton, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 30)
+    
+    self.backButton.addConstraints([backButtonHeightConstraint, backButtonWidthConstraint])
+    self.view.addConstraints([backButtonLeftConstraint, backButtonTopConstraint])
+    
+    // Create and add constraints for settingsButton
+    
+    self.settingsButton.translatesAutoresizingMaskIntoConstraints = false
+    
+    let settingsButtonRightConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.settingsButton, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Right, multiplier: 1, constant: self.minorMargin * -1)
+    
+    let settingsButtonTopConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.settingsButton, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: self.statusBarFrame.height + self.minorMargin)
+    
+    let settingsButtonHeightConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.settingsButton, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 30)
+    
+    let settingsButtonWidthConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.settingsButton, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 30)
+    
+    self.settingsButton.addConstraints([settingsButtonHeightConstraint, settingsButtonWidthConstraint])
+    self.view.addConstraints([settingsButtonRightConstraint, settingsButtonTopConstraint])
+    
+    // Create and add constraints for logOutButton
+    
+    self.logOutButton.translatesAutoresizingMaskIntoConstraints = false
+    
+    let logOutButtonRightConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logOutButton, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Right, multiplier: 1, constant: self.majorMargin * -1)
+    
+    let logOutButtonBottomConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logOutButton, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: self.majorMargin * -1)
+    
+    let logOutButtonLeftConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logOutButton, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: self.majorMargin)
+    
+    let logOutButtonHeightConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.logOutButton, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 65)
+    
+    self.logOutButton.addConstraint(logOutButtonHeightConstraint)
+    self.view.addConstraints([logOutButtonLeftConstraint, logOutButtonBottomConstraint, logOutButtonRightConstraint])
+    
+    // Create and add constraints for careersScrollView
+    
+    self.careersScrollView.translatesAutoresizingMaskIntoConstraints = false
+    
+    let careersScrollViewRightConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.careersScrollView, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Right, multiplier: 1, constant: self.majorMargin * -1)
+    
+    let careersScrollViewBottomConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.careersScrollView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self.logOutButton, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: self.majorMargin * -1)
+    
+    let careersScrollViewLeftConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.careersScrollView, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: self.majorMargin)
+    
+    let careersScrollViewTopConstraint:NSLayoutConstraint = NSLayoutConstraint.init(item: self.careersScrollView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self.profilePictureImageView, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: self.majorMargin)
+    
+    self.view.addConstraints([careersScrollViewLeftConstraint, careersScrollViewBottomConstraint, careersScrollViewRightConstraint, careersScrollViewTopConstraint])
     
   }
 
