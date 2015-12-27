b9bac8d20d6ff193206f977625835b1b55ed3b88
jcraw20 <jon@appside.co.uk>
2015-12-27 10:03:54 +0000

    BUG FIX 2

---
+++ Break in2/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  
+//
+//  Created by Jonathan Crawford on 20/12/2015.
+//
+//
+
+import Foundation
+import CoreData
+
+
+class Person: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
