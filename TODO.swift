b8cd67dba85d1d482fe1f8a68a7573c2fa38ee4a
jcraw20 <jon@appside.co.uk>
2015-12-27 10:03:39 +0000

    BUG FIX + TO DO LIST
    ALL, I’VE ADDED A TODO LIST AT THE TOP OF THE XCODE PROJ

---
+++ TODO.swift
@@ -0,0 +1,104 @@
+//
+//  File.swift
+//  Break in2
+//
+//  Created by Jonathan Crawford on 27/12/2015.
+//  Copyright © 2015 Appside. All rights reserved.
+//
+
+import Foundation
+
+//-----------------------------------------------------------------------------------------
+// GENERAL
+//-----------------------------------------------------------------------------------------
+
+/*
+
+- IMPLEMENT NOTIFICATIONS (WHEN APPROACHING A DEADLINE; FOR US TO SEND MESSAGES TO USERS)
+- RECORD VIDEO OF APP BEING USED AND SEND TO ANDRE
+- WORK OUT PAINT2CODE
+- ANIMATIONS BETWEEN VIEWS (MAYBE FADE IN/FADE OUT WORKS BETTER..)
+- WORK OUT & APPLY FREEMIUM (COINS, SUBSCRIPTION, ONE OFF PURCHASE?)
+- MARKETING STRATEGY
+- RESEARCH COMPETITIONS TO ENTER THIS INTO (SMALL SCALE)
+
+*/
+
+//-----------------------------------------------------------------------------------------
+// LOGIN
+//-----------------------------------------------------------------------------------------
+
+/*
+
+- INTRODUCTORY TUTORIAL
+- LOGIN WITH PUBLISH PERMISSIONS (TO ALLOW USERS TO POST SCORES ON FBK)
+- LOADING ANIMATIONS
+- CHECK THAT CORE DATA IS BEING SUITABLY DELETED, IF NOT SORT IT OUT!!
+- APP WALKTHROUGH UPON INITIAL LOGIN
+
+*/
+
+//-----------------------------------------------------------------------------------------
+// HOME
+//-----------------------------------------------------------------------------------------
+
+/*
+
+- DESIGN CAREER DEADLINE VIEW
+- QUERY CAREERS FROM PARSE BASED ON SELECTIONS FROM SETTINGS
+- SCALE CALENDAR BASED ON SCREEN SIZE (I.E. FONT SIZE)
+
+*/
+
+//-----------------------------------------------------------------------------------------
+// SETTINGS
+//-----------------------------------------------------------------------------------------
+
+/*
+
+- IMPLEMENT NEW CAREERS SELECTION DESIGN
+- USE CORE DATA OR NSUSERDEFAULTS TO SAVE SELECTIONS
+- VERSION UPGRADE
+- HELP
+- ABOUT
+- SETTINGS (SUCH AS TURNING OFF NOTIFICATIONS)
+- DEACTIVATE WARNING ALERT
+- DEACTIVATE LOADING DISPLAY
+- DELETE PROPERLY FROM CORE DATA
+
+*/
+
+//-----------------------------------------------------------------------------------------
+// TEST SELECTION
+//-----------------------------------------------------------------------------------------
+
+/*
+
+- EXPLANANTION OF HOW TO DO EACH TEST
+- HAVE "E,M,H" ABOVE THE TIME DESCRIPTION AND USE THAT DESCRIPTION TO DEFINE TEST PARAMETERS
+- ANIMATION BETWEEN TEST SELECTION SHOULD BE A LOADING SCREEN
+
+*/
+
+//-----------------------------------------------------------------------------------------
+// TESTS
+//-----------------------------------------------------------------------------------------
+
+/*
+
+- ARITHMETIC
+- LOGICAL
+
+*/
+
+//-----------------------------------------------------------------------------------------
+// STATISTICS
+//-----------------------------------------------------------------------------------------
+
+/*
+
+- STORE DATA IN PARSE BY TEST TYPE (I.E. NUMERICAL, VERBAL ETC...)
+- IMPLEMENT DESIGN
+- DISPLAY THIS TO USER ON TEST SELECTION
+
+*/
